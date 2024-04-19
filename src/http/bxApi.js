445c69c133eb55acb47595b0f274e1f4407b562d
zora10 <39485272+zora10@users.noreply.github.com>
2024-04-20 00:44:24 +0300

    five

--- src/http/bxApi.js
+++ src/http/bxApi.js
@@ -1,18 +1,13 @@
 
 export const dealAdd = (sendName, sendNumber, code, brand, name, grip, price, count,) => {
-    const contactData = {
-        fields: {
-            NAME: `${sendName}`,
-            PHONE: [{ VALUE: `+${sendNumber}`, VALUE_TYPE: 'WORK' }]
-        }
-    }
+    
 
     const telegram_api = '7086708799:AAFVV5zVfG1QnMen-Io1EG_mIsFon9EpJ5A';
     const id_tg = '5359516739';
     const sendAPI = `https://api.telegram.org/bot${telegram_api}/sendMessage`
     const texttt = `Заявка с сайта\nИмя : ${sendName}\nНомер : ${sendNumber}\nАртикул : ${code}\nТовар : ${name}\nФирма : ${brand}\nКатегория : ${grip}\nЦена : ${price}\nКоличество : ${count}`
     try {
-        const resq = fetch(sendAPI,{
+        fetch(sendAPI,{
             method : 'POST',
             headers : {
                 'Content-type' : 'application/json'
@@ -39,7 +34,7 @@ export const callAdd = (sendName, sendNumber) => {
     const sendAPI = `https://api.telegram.org/bot${telegram_api}/sendMessage`
     const texttt = `Заявка с сайта\nИмя : ${sendName}\nНомер : ${sendNumber}`
     try {
-        const resq = fetch(sendAPI,{
+        fetch(sendAPI,{
             method : 'POST',
             headers : {
                 'Content-type' : 'application/json'
@@ -58,18 +53,13 @@ export const callAdd = (sendName, sendNumber) => {
 
 
 export const formAdd = (sendNumber) => {
-    const contactData = {
-        fields: {
-            NAME: 'Без имени',
-            PHONE: [{ VALUE: `+${sendNumber}`, VALUE_TYPE: 'WORK' }]
-        }
-    }
+    
     const telegram_api = '7086708799:AAFVV5zVfG1QnMen-Io1EG_mIsFon9EpJ5A';
     const id_tg = '5359516739';
     const sendAPI = `https://api.telegram.org/bot${telegram_api}/sendMessage`
     const texttt = `Заявка с сайта\nИмя : Без Имени\nНомер : ${sendNumber}`
     try {
-        const resq = fetch(sendAPI,{
+        fetch(sendAPI,{
             method : 'POST',
             headers : {
                 'Content-type' : 'application/json'
